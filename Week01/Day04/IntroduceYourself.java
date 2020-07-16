b79543137a3eab4964ca54e0d1c3d75ec889d965
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-07-16 14:43:47 +0200

    Exercise IntroduceYourself

---
+++ Week01/Day04/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Anikó Pepó-Fábián");
+        System.out.println("29");
+        System.out.println("1.60");
+    }
+}

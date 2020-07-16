74c65079a79ad6141bb32dba430a273ca4202997
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-07-16 15:09:55 +0200

    Exercise TwoNumbers

---
+++ Week01/Day04/TwoNumbers.java
@@ -0,0 +1,23 @@
+public class TwoNumbers {
+    public static void main(String[] args) {
+        int a, b;
+        a = 22;
+        b = 13;
+        int c, d, e, f, g;
+        float h;
+        float x = a;
+        float y = b;
+        c = a + b;
+        d = a - b;
+        e = a * b;
+        f = a / b;
+        g = a % b;
+        h = x / y;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(h);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}

51ffb151dd052327a7bb2276aed373662fe8f31f
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-07-16 14:22:50 +0200

    Exercise HelloOthers

---
+++ Week01/Day04/HelloOthers.java
@@ -0,0 +1,8 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Zsófi!");
+        System.out.println("Hello, Erik!");
+        System.out.println("Hello, Rita!");
+    }
+
+}

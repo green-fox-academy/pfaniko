641a71bb16163bf5f39c3ac575c29ae7e7300f89
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-07-17 14:48:33 +0200

    Exercise IWontCheatOnTheExams

---
+++ Week01/Day05/src/IWontCheatOnTheExams.java
@@ -0,0 +1,9 @@
+public class IWontCheatOnTheExams {
+  public static void main(String[] args) {
+    int i = 1;
+    while (i <= 100) {
+      System.out.println(i + ". I wont cheat on the exams");
+      i++;
+    }
+  }
+}

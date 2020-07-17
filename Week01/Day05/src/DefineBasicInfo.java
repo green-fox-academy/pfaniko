1daade73ab9b7369a05ccb781e98017cea26c7d5
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-07-17 11:16:23 +0200

    Exercise DefineBasicInfo

---
+++ Week01/Day05/src/DefineBasicInfo.java
@@ -0,0 +1,12 @@
+public class DefineBasicInfo {
+  public static void main(String[] args) {
+    String Name = "Anikó Pepó-Fábián";
+    int Age = 29;
+    double Height = 1.6;
+    boolean MarriageStatus = true;
+    System.out.println(Name);
+    System.out.println(Age);
+    System.out.println(Height);
+    System.out.println(MarriageStatus);
+  }
+}

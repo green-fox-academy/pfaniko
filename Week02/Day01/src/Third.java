e564f8f3ab1fb16f9390875f73745a2e851e9bd1
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-07-20 13:39:53 +0200

    Exercise Third

---
+++ Week02/Day01/src/Third.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Third {
+
+// - Create an array variable named `numbers`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `numbers`
+
+    public static void main(String[] args) {
+        int [] numbers = {4,5,6,7};
+        System.out.println(Arrays.toString(numbers));
+        System.out.println("The third element is: " + numbers [2]);
+    }
+}

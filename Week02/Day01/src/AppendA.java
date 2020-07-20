62dbc06e83e88a76ff8225fa9372264bf013b4bf
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-07-20 16:41:43 +0200

    Exercise AppendA

---
+++ Week02/Day01/src/AppendA.java
@@ -0,0 +1,15 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import java.util.Arrays;
+
+public class AppendA {
+  public static void main(String[] args) {
+// - Create an array variable named `animals`
+//   with the following content: `["koal", "pand", "zebr"]`
+// - Add all elements an `"a"` at the end
+    String[] animals = {"koal", "pand", "zebr"};
+    for (int i = 0; i < animals.length; i++) {
+      animals [i]=animals[i] +"a";
+    }
+    System.out.println(Arrays.deepToString(animals));
+  }
+}

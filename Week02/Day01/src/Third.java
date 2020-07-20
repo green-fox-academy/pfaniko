abf82d15b65292c65936ef4ec9ea65c127c68d14
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-07-20 13:56:06 +0200

    Exercise CompareLength

--- Week02/Day01/src/Third.java
+++ Week02/Day01/src/Third.java
@@ -6,9 +6,9 @@ public class Third {
 //   with the following content: `[4, 5, 6, 7]`
 // - Print the third element of `numbers`
 
-    public static void main(String[] args) {
-        int [] numbers = {4,5,6,7};
-        System.out.println(Arrays.toString(numbers));
-        System.out.println("The third element is: " + numbers [2]);
-    }
+  public static void main(String[] args) {
+    int[] numbers = {4, 5, 6, 7};
+    System.out.println(Arrays.toString(numbers));
+    System.out.println("The third element is: " + numbers[2]);
+  }
 }

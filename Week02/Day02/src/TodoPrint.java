913855d9108a5bd7a55bb311456819db44e817a0
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-07-21 13:09:20 +0200

    Exercise TodoPrint

---
+++ Week02/Day02/src/TodoPrint.java
@@ -0,0 +1,21 @@
+public class TodoPrint {
+  public static void main(String[] args) {
+    String todoText = " - Buy milk\n";
+    // Add "My todo:" to the beginning of the todoText
+    // Add " - Download games" to the end of the todoText
+    // Add " - Diablo" to the end of the todoText but with indention
+
+    // Expected output:
+
+    // My todo:
+    //  - Buy milk
+    //  - Download games
+    //      - Diablo
+    todoText = "My todo:\n" + todoText;
+    String text1 = " - Download Games\n";
+    String text2 = " - Diablo\n";
+    String indent = "    ";
+    todoText = todoText.concat(text1).concat(indent).concat(text2);
+    System.out.println(todoText);
+  }
+}

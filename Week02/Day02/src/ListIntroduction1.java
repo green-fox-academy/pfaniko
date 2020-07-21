51ff716fc87fd918dbd5da7cdb1a705da021c6d1
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-07-21 15:27:45 +0200

    Exercise ListIntroduction1

--- Week02/Day02/src/ListIntroduction1.java
+++ Week02/Day02/src/ListIntroduction1.java
@@ -1,1 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+public class ListIntroduction1 {
+  public static void main(String[] args) {
+    ArrayList<String> names = new ArrayList<String>();
+    System.out.println(names.size());
+    names.add("William");
+    System.out.println(names.isEmpty());
+    names.add("John");
+    names.add("Amanda");
+    System.out.println(names.size());
+    System.out.println(names.get(2));
+    for (int i = 0; i < names.size(); i++) {
+      System.out.println(names.get(i));
+    }
+    for (int i = 0; i < names.size(); i++) {
+      System.out.println(i + 1 + ". " + names.get(i));
+    }
+    names.remove(1);
+    for (int i = names.size() - 1; i >= 0; i--) {
+      System.out.println(names.get(i));
+    }
+    names.removeAll(names);
+    System.out.println(names.isEmpty());
+  }
+}

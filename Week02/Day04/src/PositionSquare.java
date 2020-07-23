84a9629c2246b5bcf3da67554fb4a6055a5716c1
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-07-23 16:47:12 +0200

    Exercise PositionSquare

---
+++ Week02/Day04/src/PositionSquare.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+import javax.swing.*;
+
+import java.awt.*;
+
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PositionSquare {
+
+  public static void mainDraw(Graphics graphics) {
+    // create a function that draws one square and takes 3 parameters:
+    // the x and y coordinates of the square's top left corner and the graphics
+    // and draws a 50x50 square from that point.
+    // draw 3 squares with that function.
+    // avoid code duplication.
+
+    int x;
+    int y;
+    int limitWidth = WIDTH - 50;
+    int limitHeight = HEIGHT - 50;
+    Random random = new Random();
+    for (int i = 0; i < 3; i++) {
+      x = random.nextInt(limitWidth);
+      y = random.nextInt(limitHeight);
+      drawARectangle(graphics, x, y);
+    }
+  }
+
+  public static void drawARectangle(Graphics graphics, int x, int y) {
+    int width = 50;
+    int height = 50;
+    graphics.drawRect(x, y, width, height);
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 320;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+
+}

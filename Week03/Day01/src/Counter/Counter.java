ff801a86b120f38bfef717930481fe92e6dadb99
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-07-27 15:57:30 +0200

    Exercise Counter

---
+++ Week03/Day01/src/Counter/Counter.java
@@ -0,0 +1,26 @@
+package Counter;
+
+public class Counter {
+  private int defaultCounter;
+  private int counter;
+
+  public Counter(int counter) {
+    this.defaultCounter = counter;
+    this.counter = counter;
+  }
+
+  public void add(int number) {
+    this.counter = counter + number;
+  }
+
+  public void add() {
+    counter++;
+  }
+
+  public int get() {
+    return counter;
+  }
+  public void reset() {
+    counter = defaultCounter;
+  }
+}

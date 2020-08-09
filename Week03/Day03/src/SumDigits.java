f59ccf86226177974bc817d1393de54b273130d2
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-08-09 23:20:51 +0200

    Exercise Sum Digits

--- Week03/Day03/src/SumDigits.java
+++ Week03/Day03/src/SumDigits.java
@@ -2,14 +2,14 @@ public class SumDigits {
   public static void main(String[] args) {
 //    Given a non-negative integer n, return the sum of its digits recursively (without loops).
 
-    System.out.println(sumDigits(15286));
+    System.out.println(sumDigits(12345));
   }
 
   public static int sumDigits(int n) {
-    if (n <= 0 || n%10 == 0) {
+    if (n <= 0 || n % 10 == 0) {
       return 0;
     } else {
-      return n % 10 + sumDigits(n/10);
+      return n % 10 + sumDigits(n / 10);
     }
   }
 }

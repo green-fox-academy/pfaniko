64d29e5a3aa9d5a920b81a0fedff13a387c7eae1
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-08-26 14:53:15 +0200

    Hello World REST App added

--- Week06/Day03/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ Week06/Day03/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloRESTController {
 
+  @RequestMapping(value = "/greeting1")
+  @ResponseBody
+  public Greeting returnGreeting() {
+    return new Greeting(1, "Hello, World!");
+  }
+
   @RequestMapping(value = "/greeting")
   @ResponseBody
   public Greeting returnGreeting(@RequestParam String name) {

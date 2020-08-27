4f53593e4b9012c2e998371bdb3a691a14ad4fcb
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-08-27 15:15:36 +0200

    Shop Items added to the table

--- Week06/Day04/src/main/java/com/greenfoxacademy/webshop/controllers/WebShopController.java
+++ Week06/Day04/src/main/java/com/greenfoxacademy/webshop/controllers/WebShopController.java
@@ -1,13 +1,29 @@
 package com.greenfoxacademy.webshop.controllers;
 
+import com.greenfoxacademy.webshop.models.ShopItem;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebShopController {
+  List<ShopItem> shopItems = new ArrayList<>();
 
-  @RequestMapping (value="/webshop")
-  public String getWebshop() {
+  public WebShopController() {
+    shopItems
+        .add(new ShopItem("Running shoes", "Nike running shoes for everyday sport", 1000.0, 5));
+    shopItems.add(new ShopItem("Printer", "Some HP printer that will print pages", 3000.0, 2));
+    shopItems.add(new ShopItem("Coca cola", "0.5l standard coke", 25.0, 0));
+    shopItems.add(new ShopItem("Wokin", "Chicken with fired rice and WOKIN sauce", 119.0, 100));
+    shopItems.add(new ShopItem("T-shirt", "Blue with a corgi on a bike", 300.0, 1));
+  }
+
+  @GetMapping("/webshop")
+  public String getWebshop(Model model) {
+    model.addAttribute("shopItems", shopItems);
     return "webshop";
   }
 }
--- Week06/Day04/src/main/java/com/greenfoxacademy/webshop/models/ShopItem.java
+++ Week06/Day04/src/main/java/com/greenfoxacademy/webshop/models/ShopItem.java
@@ -3,6 +3,29 @@ package com.greenfoxacademy.webshop.models;
 public class ShopItem {
   private String name;
   private String description;
-  private int price;
+  private double price;
   private  int quantityOfStock;
+
+  public ShopItem(String name, String description, double price, int quantityOfStock) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.quantityOfStock = quantityOfStock;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public int getQuantityOfStock() {
+    return quantityOfStock;
+  }
 }

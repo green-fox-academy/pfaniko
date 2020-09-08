404056d4d8363899790899c3c08b9e134f322358
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-09-08 14:57:27 +0200

    Listing Todos H2 refactored

--- Week08/Day02/src/main/java/com/greenfoxacademy/todo/controller/TodoController.java
+++ Week08/Day02/src/main/java/com/greenfoxacademy/todo/controller/TodoController.java
@@ -7,9 +7,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/todo")
 public class TodoController {
   private TodoRepository repository;
 
@@ -25,6 +27,4 @@ public class TodoController {
     model.addAttribute("todos",todos);
     return "todolist";
   }
-
-
 }

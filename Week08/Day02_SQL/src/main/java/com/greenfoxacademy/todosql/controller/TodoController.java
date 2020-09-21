7c1dc2ce1357cbccdfc004882115a552c1cc21e4
Anikó Pepó-Fábián <aniko.fabian15@gmail.com>
2020-09-21 12:08:01 +0200

    extend listing function completed+

--- Week08/Day02_SQL/src/main/java/com/greenfoxacademy/todosql/controller/TodoController.java
+++ Week08/Day02_SQL/src/main/java/com/greenfoxacademy/todosql/controller/TodoController.java
@@ -2,33 +2,38 @@ package com.greenfoxacademy.todosql.controller;
 
 import com.greenfoxacademy.todosql.model.Todo;
 import com.greenfoxacademy.todosql.repository.TodoRepository;
+import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/todo")
 public class TodoController {
-  private TodoRepository repository;
+  private final TodoRepository todoRepository;
 
   @Autowired
-  public TodoController(TodoRepository repository) {
-    this.repository = repository;
+  public TodoController(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
   }
 
-  @RequestMapping(value = {"/","/list"})
-  public String list(@RequestParam(required = false) boolean isActive, Model model) {
-    if (isActive) {
-      model.addAttribute("todos",
-          repository.findAll().stream().filter(todo -> !todo.isDone()).collect(
-              Collectors.toList()));
+
+  @GetMapping(value = {"/", "/list"})
+  public String list(@RequestParam(required = false) Boolean isActive, Model model) {
+    List<Todo> actives = todoRepository.findAll().stream().filter(todo -> !todo.isDone()).collect(
+        Collectors.toList());
+    List<Todo> notActives = todoRepository.findAll().stream().filter(Todo::isDone).collect(
+        Collectors.toList());
+    if (isActive == null) {
+      model.addAttribute("todos", todoRepository.findAll());
+    } else if (isActive) {
+      model.addAttribute("todos", actives);
     } else {
-      model.addAttribute("todos", repository.findAll());
+      model.addAttribute("todos", notActives);
     }
     return "todolist";
   }
